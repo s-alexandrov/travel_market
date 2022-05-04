45c879aa18670311f4a5029d7a77c804b6e640a3
Sergey Alexandrov <s.alexandrov@ngs.ru>
2022-05-04 23:51:30 +0700

    edit constants.py

--- fixtures/constants.py
+++ fixtures/constants.py
@@ -5,6 +5,6 @@ class RegNotice:
     LOG_IN = "Log in"
     ERROR_PASSWORD = "The two password fields didn’t match."
     ERROR_AGE = "You are young!"
-    ERROR_PASSWORD_NUMERIC = "This password is too common."
+    ERROR_PASSWORD_NUMERIC = "This password is entirely numeric."
     ERROR_PASSWORD_COMMON = "This password is too common."
     ERROR_RE_REG = "A user with that username already exists."
