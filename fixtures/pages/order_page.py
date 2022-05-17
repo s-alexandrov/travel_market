499f65dd708f6b03ffa031984b7b4f3c6ac7e47f
Sergey Alexandrov <s.alexandrov@ngs.ru>
2022-05-17 16:48:09 +0700

    add order test

---
+++ fixtures/pages/order_page.py
@@ -0,0 +1,54 @@
+from selenium.webdriver.common.by import By
+from fixtures.pages.base_page import BasePage
+from fixtures.constants import LoginNotice
+
+
+class OrderPage(BasePage):
+    LOGIN_LINK = (By.XPATH, "//li[@class='nav-item'][2]/a[@class='nav-link']")
+    USERNAME_FIELD = (By.NAME, "username")
+    PASSWORD_FIELD = (By.NAME, "password")
+    LOGIN_BUTTON = (By.XPATH, "//form[@class='form-horizontal']/input[@class='form-control']")
+    LOGIN_TEXT = (By.CLASS_NAME, "dropdown-toggle")
+    SEE_ACCOMMODATION = (By.CLASS_NAME, "btn-danger")
+    BUTTON_MORE = (By.CLASS_NAME, "btn-danger")
+    BUTTON_BOOK = (By.CLASS_NAME, "btn-danger")
+    BASKET_LINK = (By.XPATH, "//a[@class='dropdown-item'][2]")
+    BOOKED_TEXT = (By.CLASS_NAME, "basket_summary")
+
+    def open_login_page(self):
+        self.open_page(self.app.url)
+        self.click(locator=self.LOGIN_LINK)
+
+    def login(self):
+        self.fill(locator=self.USERNAME_FIELD, value=LoginNotice.login)
+        self.fill(locator=self.PASSWORD_FIELD, value=LoginNotice.password)
+        self.click(locator=self.LOGIN_BUTTON)
+
+    def success_log_in_text(self) -> str:
+        element = self.text(locator=self.LOGIN_TEXT)
+        return element
+
+    def click_button_see_accommodation(self):
+        self.click(locator=self.SEE_ACCOMMODATION)
+
+    def find_button_more(self):
+        element = self.text(locator=self.BUTTON_MORE)
+        return element
+
+    def click_button_more(self):
+        self.click(locator=self.BUTTON_MORE)
+
+    def find_button_book(self):
+        element = self.text(locator=self.BUTTON_MORE)
+        return element
+
+    def click_button_book(self):
+        self.click(locator=self.BUTTON_MORE)
+
+    def open_basket_page(self):
+        self.click(locator=self.LOGIN_TEXT)
+        self.click(locator=self.BASKET_LINK)
+
+    def check_book(self):
+        element = self.text(locator=self.BOOKED_TEXT)
+        return element
---
+++ tests/test_order_page.py
@@ -0,0 +1,19 @@
+from fixtures.constants import LoginNotice
+from fixtures.constants import OrderNotice
+
+
+class TestOrderPage:
+    def test_hotel_booking(self, app):
+        """
+        Бронирование отеля.
+        """
+        app.order_page.open_login_page()
+        app.order_page.login()
+        assert app.order_page.success_log_in_text() == LoginNotice.login
+        app.order_page.click_button_see_accommodation()
+        assert app.order_page.find_button_more() == OrderNotice.BUTTON_MORE
+        app.order_page.click_button_more()
+        assert app.order_page.find_button_book() == OrderNotice.BUTTON_BOOK
+        app.order_page.click_button_book()
+        app.order_page.open_basket_page()
+        assert OrderNotice.BOOKED in app.order_page.check_book()
